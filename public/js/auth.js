4db66e116a98adc7dc1b9aac16d4729764830da0
Akshat Sharma <akshatsharma46@gmail.com>
2019-03-01 04:21:33 -0800

    password-validation for signup

--- public/js/auth.js
+++ public/js/auth.js
@@ -8,3 +8,46 @@ $('#switch').click(function(){
 	$('#background').toggleClass("two");
 	$('#image-overlay').toggleClass("two");
 })
+
+/*PASSWORD VALIDATION WITH PLAIN JS (WITHOUT JQUERY)*/
+
+var field = document.getElementsByClassName('password-field').password;
+var validation = document.getElementsByClassName('password-validate').item(0);
+
+function handler(e) {
+	if(field===document.activeElement){
+		console.log("length " + (field.value));
+		if((field.value.length+1)<2){
+			validation.className = "password-validate ";
+		}
+		if((field.value.length+1)>2){
+			validation.className = "password-validate " + "weak";
+		}
+		if((field.value.length+1)>4){
+			validation.className = "password-validate " + "med";
+		}
+		if((field.value.length+1)>8){
+			validation.className = "password-validate " + "strong";
+		}
+	}
+}
+//console.log("field: " + validation.item(0));
+document.addEventListener('keyup', handler)
+//document.attachEvent('onkeydown', handler)
+// if (document.attachEvent) document.attachEvent('onkeydown', handler);
+// else document.addEventListener('keydown', handler);
+
+function hasClass(el, className) {
+	return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+}
+
+function addClass(el, className) {
+	console.log("here");
+	if (el.classList) el.classList.add(className);
+	else if (!hasClass(el, className)) el.className += ' ' + className;
+}
+
+function removeClass(el, className) {
+	if (el.classList) el.classList.remove(className);
+	else el.className = el.className.replace(new RegExp('\\b'+ className+'\\b', 'g'), '');
+}
--- public/templates/authenticate.marko.js
+++ public/templates/authenticate.marko.js
@@ -19,11 +19,11 @@ function render(input, out, __component, component, state) {
 
   component_globals_tag({}, out);
 
-  out.w("<div id=\"background\"><div id=\"panel-box\"><div class=\"panel\"><div class=\"auth-form on\" id=\"login\"><div id=\"form-title\">Log In</div><form action=\"/login\" method=\"POST\"><input name=\"username\" type=\"text\" required=\"required\" placeholder=\"Username\"><input name=\"password\" type=\"password\" required=\"required\" placeholder=\"Password\"><button type=\"Submit\">Log In</button></form></div><div class=\"auth-form\" id=\"signup\"><div id=\"form-title\">Register</div><form action=\"/register\" method=\"POST\"><input name=\"username\" type=\"text\" required=\"required\" placeholder=\"Username\"><input name=\"password\" type=\"password\" required=\"required\" placeholder=\"Password\"><button type=\"Submit\">Sign Up</button></form></div></div><div class=\"panel\"><div id=\"switch\">Sign Up</div><div id=\"image-overlay\"></div><div id=\"image-side\"></div></div></div></div>");
+  out.w("<div id=\"background\"><div id=\"panel-box\"><div class=\"panel\"><div class=\"auth-form on\" id=\"login\"><div id=\"form-title\">Log In</div><form action=\"/login\" method=\"POST\"><input name=\"username\" type=\"text\" required=\"required\" placeholder=\"Username\"><input name=\"password\" type=\"password\" required=\"required\" placeholder=\"Password\"><button type=\"Submit\">Log In</button></form></div><div class=\"auth-form\" id=\"signup\"><div id=\"form-title\">Register</div><form action=\"/register\" method=\"POST\"><input name=\"username\" type=\"text\" required=\"required\" placeholder=\"Username\"><div class=\"center password\"><input class=\"password-field\" name=\"password\" type=\"password\" required=\"required\" placeholder=\"Password\"><div class=\"password-validate\"></div></div><button type=\"Submit\">Sign Up</button></form></div></div><div class=\"panel\"><div id=\"switch\">Sign Up</div><div id=\"image-overlay\"></div><div id=\"image-side\"></div></div></div></div>");
 
   init_components_tag({}, out);
 
-  await_reorderer_tag({}, out, __component, "25");
+  await_reorderer_tag({}, out, __component, "27");
 
   out.w("</body><script src=\"https://code.jquery.com/jquery-3.3.1.min.js\"></script><script src=\"/js/auth.js\"></script></html>");
 }
