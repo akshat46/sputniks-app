5744a7b49a4697273268758edd5c8311958a288b
Jay Patel <pateljay134@gmail.com>
2019-04-22 13:39:10 -0700

    PieChart option added for Wifi Chart

--- public/js/wifi_chart.js
+++ public/js/wifi_chart.js
@@ -1,20 +1,42 @@
-google.charts.load('current', {'packages':['bar']});
-// google.charts.setOnLoadCallback(drawChart);
+// google.charts.load('current', {'packages':['bar']});
+// // google.charts.setOnLoadCallback(drawChart);
+
+// function drawChart(data) {
+//     console.log("Data:", data);
+//     var data = google.visualization.arrayToDataTable([
+//         ['Wifi', 'Free Wifi', 'Paid Wifi', 'No wifi', 'No Info'],
+//         [data.city, data.free, data.yes, data.no, data.noInfo]
+//       ]);
+
+//       var options = {
+//         legend: { position: 'top', maxLines: 3 },
+//         bar: { groupWidth: '75%' },
+//         isStacked: true
+//       };
+
+//     var chart = new google.charts.Bar(document.getElementById('chart-small'));
+
+//     chart.draw(data, google.charts.Bar.convertOptions(options));
+// }
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
 
 function drawChart(data) {
-    console.log("Data:", data);
-    var data = google.visualization.arrayToDataTable([
-        ['Wifi', 'Free Wifi', 'Paid Wifi', 'No wifi', 'No Info'],
-        [data.city, data.free, data.yes, data.no, data.noInfo]
-      ]);
 
-      var options = {
-        legend: { position: 'top', maxLines: 3 },
-        bar: { groupWidth: '75%' },
-        isStacked: true
-      };
+  var displayData = google.visualization.arrayToDataTable([
+    ['WiFi Group', 'Number of Restaurants'],
+    ['Free Wifi', data.free],
+    ['Paid Wifi', data.yes],
+    ['No wifi', data.no],
+    ['No Info', data.noInfo]
+  ]);
 
-    var chart = new google.charts.Bar(document.getElementById('chart-small'));
+  var options = {
+    title: 'WiFi options in '+ data.city,
+  };
 
-    chart.draw(data, google.charts.Bar.convertOptions(options));
+  var chart = new google.visualization.PieChart(document.getElementById('chart-small'));
+
+  chart.draw(displayData, options);
 }
